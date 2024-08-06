a22a813c3f2ffb7123d42c062689e8ab618be9cf
Geronimo Dimarco <unzagageronimo@gmail.com>
2024-08-06 20:53:49 -0300

    ending the proyect

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { NumericFormat } from 'react-number-format';
 
 export default function ListadoEmpleados() {
@@ -19,6 +20,11 @@ export default function ListadoEmpleados() {
         setEmpleados(resultado.data)
     }
 
+    const eliminarEmpleado = async (id) => {
+        await axios.delete(`${urlBase}/${id}`);
+        cargarEmpleados();
+    }
+
     return (
         <div className='container'>
             <div className='container text-center' style={{margin: "30px"}}>
@@ -32,6 +38,7 @@ export default function ListadoEmpleados() {
                 <th scope="col">Empleado</th>
                 <th scope="col">Departamento</th>
                 <th scope="col">Sueldo</th>
+                <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -46,6 +53,16 @@ export default function ListadoEmpleados() {
                         displayType={'text'}
                         thousandSeparator=',' prefix={'$'}
                         decimalScale={2} fixedDecimalScale/>
+                        </td>
+                        <td className='text-center'>
+                            <div>
+                                <Link to={`/editar/${empleado.idEmpleado}`}
+                                className= 'btn btn-warning btn-sm me-3'>Editar</Link>
+                                <button onClick={()=> eliminarEmpleado(empleado.idEmpleado)}
+                                    className='btn btn-danger btn-sm'
+                                    >Eliminar
+                                </button>
+                            </div>
                         </td>
                     </tr>
                     ))
